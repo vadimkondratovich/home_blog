5a1140023f0812acdedac9d0f2c1f12af2aaacfa
Vadim <vadim.kondratovich1993@gmail.com>
2021-09-09 15:57:06 +0300

    white_screen_and_list_of_links

---
+++ home_blog/blog/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def blog_page(request):
+    return render(request, "all_blogs.html", {})
--- home_blog/home_blog/settings.py
+++ home_blog/home_blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'home_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "home_blog" / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
