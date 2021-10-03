6451ecc930a14d6053b4282e12df2d0d18b08bb6
Vadim <vadim.kondratovich1993@gmail.com>
2021-10-03 23:59:50 +0300

    all_blogs without changes in file all_blogs.html

--- home_blog/blog/views.py
+++ home_blog/blog/views.py
@@ -1,4 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponseForbidden
+from blog.forms import CreatePost
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from blog.models import Post
+from django.views.generic.edit import FormView
 
-def blog_page(request):
-    return render(request, "all_blogs.html", {})
+#def blog_page(request):
+#    return render(request, "all_blogs.html", {})
+
+
+class AllPosts(ListView):
+    paginate_by = 2
+    model = Post
+    template_name = "all_blogs.html"
+
+
+
+
+class NewPost(LoginRequiredMixin, FormView):
+    template_name = "create_blog.html"
+    form_class = CreatePost
+    login_url = '/user/login/'
+    success_url = "/blog/"
+
+    def form_valid(self, form):
+        if form.cleaned_data["author"] == self.request.user:
+            form.save()
+            return super().form_valid(form)
+        return HttpResponseForbidden() 
